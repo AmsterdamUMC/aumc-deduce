725cd55015c9ed73ee5e4cd4881af0a2f2c25ee5
Jacob Rousseau <J.B.I.Rousseau@amsterdamumc.nl>
2024-06-05 14:39:31 +0200

    Investigating weird problems in output when using conf-files from either amsterdamumc or DeduceConf project

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,4 @@ from deduce import Deduce
 
 @pytest.fixture(scope="session")
 def model():
-    return Deduce(config="../amsterdamumc_config.json", build_lookup_structs=False)
+    return Deduce(config="G:/divjk/cru/DEDUCE/GIT-share/jacob-git/DeduceConf-2/aumc_config.json", build_lookup_structs=True, load_base_config=False)
