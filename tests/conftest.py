55be736af5e074d62bf01348c97f7d91095b8e6c
Jacob Rousseau <J.B.I.Rousseau@amsterdamumc.nl>
2024-06-06 15:54:22 +0200

    Changes following the introduction of the EPIC Amsterdamumc lists to the configuraiton project DeduceConf

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,6 @@ from deduce import Deduce
 
 @pytest.fixture(scope="session")
 def model():
-    return Deduce(config="G:/divjk/cru/DEDUCE/GIT-share/jacob-git/DeduceConf-2/aumc_config.json", build_lookup_structs=True, load_base_config=False)
+    # "G:/divjk/cru/DEDUCE/GIT-share/jacob-git/amsterdamumc-deduce/amsterdamumc_config.json"
+    # "G:/divjk/cru/DEDUCE/GIT-share/jacob-git/DeduceConf/aumc_config.json"
+    return Deduce(config= "G:/divjk/cru/DEDUCE/GIT-share/jacob-git/DeduceConf/aumc_config.json", build_lookup_structs=True, load_base_config=False)
