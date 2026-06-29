506fd1c5bfe23d7cc3d90e4bf48203ae7286d7b6
Jacob Rousseau <J.B.I.Rousseau@amsterdamumc.nl>
2026-06-29 14:15:42 +0200

    Move of the location for the release numeric value to the file VERSION.txt. Addition of the script to create release candidates.

--- scripts/create-release-branch.sh
+++ scripts/create-release-branch.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 #
 # Creates a release branch for an upcoming release of DEDUCE
 #
@@ -9,10 +9,60 @@
 # 3) run all the tests and if failure then aborts this script
 # 2) creation of a branch in the aumc-deduce project with that release id called 'release-X.Y.Z'
 # 3) change of the version number in the pyproject.toml with the release id and a release candidate follow-up number of 1
-# 4) create a version tag on the 3 projjects involved: aumc-deduce, aumc-deduce-conf and aumc-supplements
 #
 #
 #
 # TODO discuss with team if we have to follow a deveop & release branching strategy for the associated projects aumc-deduce-conf and aumc-deduce-supplements
 # or only use a simliar version tag across these 2 projects
 # Add a separate script to create the version tags in the 3 associated projects'
+
+TAG_VERSION=`cat /home/$USER/git/aumc-deduce/VERSION.txt`
+TAG_VERSION=`echo "$TAG_VERSION"`
+BRANCH_NAME=`echo "release-$TAG_VERSION"`
+
+echo "Tag version specified in VERSION.txt: $TAG_VERSION"
+
+# Note: git branch does support the -C ( work across directories) option
+git branch --list | grep -q "$BRANCH_NAME"
+
+IS_BRANCH_ALREADY_PRESENT=$?
+
+if [ $IS_BRANCH_ALREADY_PRESENT == 0 ]; then
+	echo "Branch $BRANCH_NAME is already present. Skipping branch creation"
+	git checkout $BRANCH_NAME
+else
+	echo "Tag $BRANCH_NAME is not yet present. Creating branch"
+	git checkout -b $BRANCH_NAME
+	
+fi
+
+# Count the number of version tags present as git version tags 
+# to determine the next release candidate follow-up number
+
+RELEASE_CANDIDATE_COUNT=`git tag --list | grep "$TAG_VERSION-rc" | wc -l`
+echo "Number of release candidates for version $TAG_VERSION: $RELEASE_CANDIDATE_COUNT"
+RELEASE_CANDIDATE_COUNT=$((RELEASE_CANDIDATE_COUNT+1))
+NEW_RELEASE_CANDIDATE_TAG=`echo $TAG_VERSION-rc$RELEASE_CANDIDATE_COUNT`
+echo ""
+read -p "Do you want to create a new release candidate: $NEW_RELEASE_CANDIDATE_TAG? (y/n)" ANSWER
+case ${ANSWER:0:1} in
+	y|Y )
+		echo "Creating release candidate with tag: $NEW_RELEASE_CANDIDATE_TAG"
+		# replace the release candidate tag in the pyproject file
+		CURRENT_VERSION=`cat /home/$USER/git/aumc-deduce/pyproject.toml | grep 'version =' | cut -d '=' -f2| tr '"' ' ' | tr -d ' '`
+		echo "Replacing $CURRENT_VERSION in pyproject.toml with: $NEW_RELEASE_CANDIDATE_TAG"
+		CURRENT_VERSION="version = \"$CURRENT_VERSION\""
+		NEW_RELEASE_CANDIDATE_TAG="version = \"$NEW_RELEASE_CANDIDATE_TAG\""
+		sed -i -e "s/$CURRENT_VERSION/$NEW_RELEASE_CANDIDATE_TAG/g" /home/$USER/git/aumc-deduce/pyproject.toml
+		
+		# Run poetry install to ensure that the poetry.lock file matches the version tag specified in the pyproject.toml
+		poetry install
+	;;
+	* )
+		echo "Skipped creating release candidate ..."
+NEW_RELEASE_CANDIDATE_TAG ;;
+esac
+#
+
+
+
